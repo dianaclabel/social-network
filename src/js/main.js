da63aec770e7900618b14e3b51c40cbfbce42975
Jae <one.sweet.dreams@gmail.com>
2023-05-17 00:10:25 -0400

    se agrega firebase, vista de verificación, y se arreglan detalles de otros archivos

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,26 @@
 // Este es el punto de entrada de tu aplicacion
 // import { myFunction } from "./index.js";
-import { registerTemplate } from "./template/registro.js";
-import { loginTemplate } from "./template/inicio-sesion.js";
+import { registerTemplate } from './template/registro.js';
+import { loginTemplate } from './template/inicio-sesion.js';
+import { verification } from './template/verificacion.js';
 // myFunction();
-//RegistroTemplate
-const templateRegister = document.createElement("template");
+// RegistroTemplate
+const templateRegister = document.createElement('template');
 templateRegister.innerHTML = registerTemplate;
 
-document.body.classList.add("body-register");
+document.body.classList.add('body-register');
 document.body.prepend(templateRegister.content);
 
+// LoginTemplate
 
-//LoginTemplate
-
-const templateLogin = document.createElement("template");
+const templateLogin = document.createElement('template');
 templateLogin.innerHTML = loginTemplate;
-document.body.classList.add("body-login");
+document.body.classList.add('body-login');
 document.body.prepend(templateLogin.content);
+
+// Verification register
+const templateVerification = document.createElement('template');
+templateVerification.innerHTML = verification;
+
+document.body.classList.add('body-verification');
+document.body.append(templateVerification.content);
