c16aa420960a6b61cafb1309c61ed3cbc7c7ad34
Fatima <fatima_gtz@outlook.com>
2023-05-22 16:32:08 -0600

    se agrega estilos a 404

--- src/js/routes/notFound.js
+++ src/js/routes/notFound.js
@@ -1,9 +1,12 @@
 export const errorNotFound = (navegateTo) => {
   const sectionEl = document.createElement("section");
   sectionEl.innerHTML = /* html */ `
-         <p>No llores solo es un </p>
-        <img src="" alt="logo-404">
-        <p>déjame guiarte de <b>regreso Home</b></p>
+         <p class ="parrafoUno">No llores solo es un </p>
+         <div id ="logo">
+        <img  class ="zanahoria" src="../../image/zanahoria-kawai.jpg" alt=>
+        <img class ="numero" src="../../image/404final.png" alt="">
+        </div>
+        <p class ="parrafoDos">Déjame guiarte de <b class = "parrafoTres">regreso Home</b></p>
     `;
 
   return sectionEl;
