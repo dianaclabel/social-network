f970c84981ca2714d1409a56bcaa19f20bf735b0
Jae <one.sweet.dreams@gmail.com>
2023-05-26 20:16:44 -0400

    Se agregan cambios en el muro(home/homeDos), stilos css para muro

--- src/js/routes/home.js
+++ src/js/routes/home.js
@@ -1,25 +1,89 @@
-import { firebaseUser } from '../firebase';
+// import { firebaseUser } from '../firebase';
 
-export const home = (navigateTo) => {
-  const homeNodo = document.createElement('header');
+export const header = () => {
+  const headerNodo = document.createElement('header');
+  const divContenedorHeader = document.createElement('div');
   const imgLogo = document.createElement('img');
   const imgBell = document.createElement('img');
+  const imgUserPhoto = document.createElement('img');
 
-  homeNodo.id = 'homePrincipal';
-  homeNodo.className = 'homePrincipal';
+  divContenedorHeader.className = 'contenedor-header';
 
-  imgLogo.className = 'homeLogo';
+  headerNodo.id = 'hederPrincipal';
+  headerNodo.className = 'hederPrincipal';
+
+  imgLogo.className = 'hederLogo';
   imgLogo.src = '../../image/logo.png';
   imgLogo.alt = 'logoFatFit';
 
-  imgBell.className = 'campana';
+  imgBell.className = 'iconHeader';
   imgBell.src = '../../icon/bell.png';
   imgBell.alt = 'campana-notificacion';
 
-  homeNodo.appendChild(imgLogo);
-  homeNodo.appendChild(imgBell);
+  imgUserPhoto.className = 'iconHeader';
+  imgUserPhoto.src = '../../image/usuarioCarrot.png';
+  imgUserPhoto.id = 'userPhoto';
 
-  document.body.appendChild(homeNodo);
-  document.body.appendChild(imgLogo);
-  document.body.appendChild(imgBell);
+  divContenedorHeader.appendChild(imgLogo);
+  divContenedorHeader.appendChild(imgBell);
+  divContenedorHeader.appendChild(imgUserPhoto);
+  headerNodo.appendChild(divContenedorHeader);
+
+  // document.body.append(headerNodo);
+
+  return headerNodo;
+};
+
+export const footer = () => {
+  const footerNodo = document.createElement('footer');
+  footerNodo.className = 'footerHome';
+
+  const divContainerMenu = document.createElement('div');
+  divContainerMenu.className = 'container-menu';
+
+  const imgIconHome = document.createElement('img');
+  imgIconHome.src = '../../icon/homepage.png';
+  imgIconHome.className = 'iconFooter';
+  imgIconHome.id = 'iconHome';
+  imgIconHome.alt = 'icon-Home';
+  divContainerMenu.appendChild(imgIconHome);
+
+  const imgIconSearch = document.createElement('img');
+  imgIconSearch.src = '../../icon/Search.png';
+  imgIconSearch.alt = 'icon-search';
+  imgIconSearch.className = 'iconFooter';
+  imgIconSearch.id = 'iconSearch';
+  divContainerMenu.appendChild(imgIconSearch);
+
+  const imgIconAdd = document.createElement('img');
+  imgIconAdd.src = '../../icon/Add.png';
+  imgIconAdd.alt = 'icon-Add';
+  imgIconAdd.className = 'iconFooter';
+  imgIconAdd.id = 'iconAdd';
+  divContainerMenu.appendChild(imgIconAdd);
+
+  const imgIconLikes = document.createElement('img');
+  imgIconLikes.src = '../../icon/carrot.png';
+  imgIconLikes.alt = 'icon-Likes';
+  imgIconLikes.className = 'iconFooter';
+  imgIconLikes.id = 'iconLikes';
+  divContainerMenu.appendChild(imgIconLikes);
+
+  const imgIconLogout = document.createElement('img');
+  imgIconLogout.src = '../../icon/Logout.png';
+  imgIconLogout.alt = 'logout';
+  imgIconLogout.className = 'iconFooter';
+  imgIconLogout.id = 'logout';
+  divContainerMenu.appendChild(imgIconLogout);
+
+  footerNodo.appendChild(divContainerMenu);
+
+  // document.body.append(footerNodo);
+
+  return footerNodo;
+};
+
+export const home = () => {
+  header();
+  footer();
 };
--- src/js/routes/homeDos.js
+++ src/js/routes/homeDos.js
@@ -1,24 +1,53 @@
-import { firebaseUser } from '../firebase';
+/* eslint-disable max-len */
+// import { firebaseUser } from '../firebase';
 
-export const home = () => {
-  const sectionEl = document.createElement('section');
-  sectionEl.innerHTML = /* html */ `      
-      <header class="header-login">
-           <img  class="logo-register" src="../../image/logo.png" alt="logo">
-      </header>
-       <main class="main-verification">
-           <h1 class="titleGreetings"> home</h1>    
-           <h2 > Tedamos la bienvenida <span id="user-name"></span></h2>  
-           
-       </main>
-      `;
-  const name = sectionEl.querySelector('#user-name');
+// export const home = () => {
+//   const sectionEl = document.createElement('section');
+//   sectionEl.innerHTML = /* html */ `
+//        <header class="headerNodo">
+//          <div class="contenedor-header">
+//          <img src="../../image/logo.png" alt="hederLogo" class="hederLogo">
+//          <img src="../../icon/bell.png" alt="campanita" class="iconHeader" id="bell">
+//          <img src="../../image/usuarioCarrot.png" alt="usuario" class="iconHeader" id="userPhoto">
+//          </div>
+//        </header>
+//         <section class="section-header">
+//             <h2 > Bienvenida <span id="user-name"></span></h2>
+//              <br>
+//             <textarea id="postContent" placeholder="Publicamos algo?..." cols="30" rows="10"></textarea>
+//             <br>
+//             <button type="submit">Publicar</button>
+//         </section>
+//         <div id="post-container"><!-- Las publicaciones se agregarán aquí --></div>
+//         <footer class="footerHome">
+//          <div class="container-menu">
+//          <img src="../../icon/homepage.png" alt="icon-Home" class="iconFooter" id="iconHome">
+//          <img src="../../icon/Search.png" alt="icon-search" class="iconFooter" id="iconSearch">
+//          <img src="../../icon/Add.png" alt="icon-Add" class="iconFooter" id="iconAdd">
+//          <img src="../../icon/carrot.png" alt="icon-Likes" class="iconFooter" id="iconLikes">
+//          <img src="../../icon/Logout.png" alt="logout" class="iconFooter" id="logout">
+//          </div>
+//         </footer>
+//        `;
+//   const name = sectionEl.querySelector('#user-name');
+//   const user = firebaseUser();
+//   if (user) {
+//     name.textContent = user.displayName;
+//   }
+//   return sectionEl;
+// };
 
-  const user = firebaseUser();
+// export const home = () => {
+//   const headerNodo = document.createElement('header');
+//   headerNodo.id = 'hederPrincipal';
+//   headerNodo.className = 'hederPrincipal';
 
-  if (user) {
-    name.textContent = user.displayName;
-  }
+//   const imgLogo = document.createElement('img');
+//   imgLogo.src = '../../image/logo.png';
 
-  return sectionEl;
-};
+//   headerNodo.append(imgLogo);
+
+//   // document.root.appendChild(headerNodo);
+
+//   return headerNodo;
+// };
