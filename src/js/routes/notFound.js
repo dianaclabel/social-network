650417698651c8a3c39bfa6c68142e040f6255ff
Jae <one.sweet.dreams@gmail.com>
2023-06-06 19:23:41 -0400

    Se arreglan detalles de nodos

--- src/js/routes/notFound.js
+++ src/js/routes/notFound.js
@@ -2,8 +2,8 @@ export const errorNotFound = (navegateTo) => {
   const sectionEl = document.createElement('section');
   sectionEl.innerHTML = /* html */ `
         <div class = "fondo">
-         <p class ="parrafoUno">No llores solo es un </p>
-         <img class = "numero" src="../../image/404zanahoria.png" alt="">
+          <p class ="parrafoUno">No llores solo es un </p>
+          <img class = "numero" src="../../image/404zanahoria.png" alt="">
         <p class ="parrafoDos">Déjame guiarte de <a href='/feed' class ="parrafoTres">regreso Home</a></p>
 </div>
 `;
